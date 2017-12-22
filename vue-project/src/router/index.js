95d81eec5fd62af8214c330592cfc8964862ae2f
zzt <1315691850@qq.com>
2017-12-22 21:46:14 +0800

    add the home page

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import login from '@/components/login'
+import home from '@/components/home'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -8,8 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login_component',
+      component: login
+    },
+    {
+      path: '/home',
+      name: 'home_component',
+      component: home
     }
   ]
 })
