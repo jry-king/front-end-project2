dfb8b1038fec28278ca962782a9365d516e61fe0
zzt <1315691850@qq.com>
2017-12-24 17:35:52 +0800

    add the Like and Dislike

--- userpage/vue-project/src/components/food_review.vue
+++ userpage/vue-project/src/components/food_review.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
       <img src="../assets/main.png">
+      <button class="back_button" v-on:click='back()'>Back to home</button>
     </div>
 </template>
 <script>
@@ -8,8 +9,21 @@
         name: 'Food_review',
         data () {
             return {}
-        }
+        },
+      methods:{
+            back:function () {
+              this.$router.push('/home')
+            }
+      }
     }
 </script>
 <style>
+  .back_button {
+    display:inline;
+    font-size: 14px;
+    width: 100px;
+    color: red;
+    padding: 0 5px;
+    cursor: pointer;
+  }
 </style>
