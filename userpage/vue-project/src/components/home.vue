b18cdbd8429f54060f188d903978492b601c5022
zzt <1315691850@qq.com>
2017-12-25 19:16:50 +0800

    delete rubbish

--- userpage/vue-project/src/components/home.vue
+++ userpage/vue-project/src/components/home.vue
@@ -38,13 +38,8 @@
       </el-menu>
     </el-aside>
     <el-container>
-      <el-header v-show="showcascader"style="text-align: left; font-size: 20px">
-        <el-cascader
-          :options="options"
-          v-model="selectedOptions3"
-        ></el-cascader>
+      <el-header style="text-align: left; font-size: 20px">
       </el-header>
-
       <el-dropdown @command="handleCommand" placement="top">
         <span class="el-dropdown-link">
           下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
@@ -152,44 +147,8 @@
         牛肉拉面: false,
         薯条: false,
         臭豆腐: false,
-        showcascader: false,
         input:'',
         tableData: Array(20).fill(item),
-        options: [
-          {value: '食堂', label: '食堂',
-            children: [
-              {value: 'basic', label: '四餐',
-                children: [
-                  {value: 'layout', label: '甜魔烘培'},
-                  {value: 'icon', label: '成都小菜'},
-                  {value: 'button', label: '唐汇'},
-                  {value: 'button', label: '韩式鸟叔炸鸡'},
-                  {value: 'button', label: '泰式咖喱'},
-                  {value: 'button', label: '港式滑蛋饭'},
-                  {value: 'button', label: '顺德风味'},
-                  {value: 'button', label: '麻辣香锅'},
-                  {value: 'button', label: '广式拉肠粉'},
-                  {value: 'button', label: '风味小吃'},
-                  {value: 'button', label: '河南烩面'}
-                ]},
-              {value: 'form', label: '二餐',},
-              {value: 'data', label: '三餐',},
-              {value: 'notice', label: '一餐',},
-              {value: '五餐', label: '五餐',},
-              {value: 'others', label: '六餐',}
-            ]},
-          {value: 'resource', label: '菜系', children: [{
-            value: 'axure',
-            label: '本帮菜'
-          },
-            {value: 'sketch', label: '川菜'},
-            {value: 'docs', label: '西餐', children:[
-              {value: 'layout', label: '二餐时光塔'},
-              {value: 'jimu', label: '四餐吉姆利德',},
-              {value:'jimu1', label:'五餐吉姆利德'},
-              {value:'liu', label:'六餐'}]
-            }]
-          }],
         selectedOptions3: ['食堂', '菜系'],
         activeNames: ['1']
       }
@@ -219,14 +178,14 @@
   }
   .demo-input-label {
     display: inline-block;
-    width: 130px;
+    width: 100px;
   }
   .el-carousel-item h3 {
     color: #475669;
     font-size: 14px;
     opacity: 0.75;
     line-height: 200px;
-    height:300px;
+    height:30px;
     margin: 0;
   }
 
